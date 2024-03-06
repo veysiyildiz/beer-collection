22170db78c7b867a78a0186dc0d231b6e5c43f61
Veysi YILDIZ <veysiyildiz@gmail.com>
2024-03-06 03:32:40 +0300

    Update BottomBar.tsx

--- src/components/organisms/BottomBar.tsx
+++ src/components/organisms/BottomBar.tsx
@@ -4,7 +4,7 @@ import { MenuItem } from "@/components/molecules";
 
 const BottomBar: React.FC = () => (
   <section className="bg-white  border-t border-r fixed bottom-0 z-10 w-full rounded-t-3xl p-4 sm:px-7 md:hidden">
-    <div className="flex items-center justify-center gap-3 sm:gap-5">
+    <nav className="flex items-center justify-center gap-3 sm:gap-5">
       {sidebarLinks.map((link) => {
         return (
           <MenuItem
@@ -15,7 +15,7 @@ const BottomBar: React.FC = () => (
           />
         );
       })}
-    </div>
+    </nav>
   </section>
 );
 
