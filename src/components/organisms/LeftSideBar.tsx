904e9d211f3f520363714ac9a852106a872d8779
Veysi YILDIZ <veysiyildiz@gmail.com>
2024-03-06 03:32:16 +0300

    Update LeftSideBar.tsx

--- src/components/organisms/LeftSideBar.tsx
+++ src/components/organisms/LeftSideBar.tsx
@@ -4,7 +4,7 @@ import { sidebarLinks } from "@/lib/constants";
 
 const LeftSideBar: React.FC = () => (
   <section className="sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-between overflow-auto border-r pb-5 pt-28 max-md:hidden">
-    <div className="flex w-full flex-1 flex-col gap-6 px-6">
+    <nav className="flex w-full flex-1 flex-col gap-6 px-6">
       {sidebarLinks.map((link) => {
         return (
           <MenuItem
@@ -15,7 +15,7 @@ const LeftSideBar: React.FC = () => (
           />
         );
       })}
-    </div>
+    </nav>
   </section>
 );
 
